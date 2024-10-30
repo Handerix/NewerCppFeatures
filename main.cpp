3c5d38bb48f2b11c254916ad3fbd66514c905ff0
Handerix <robert.gawlik1996@gmail.com>
2024-10-31 00:38:45 +0100

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+
+int main()
+{
+    int x = 7;
+    if (int y = 2 * x; y > 10)
+    {
+        std::cout << "y: " << y;
+    }
+
+    std::cout << "Hello, World!";
+    return 0;
+}
